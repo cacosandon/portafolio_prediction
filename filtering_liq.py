25ac8ccf0b484bf08f044881ac22d468a4efdbf3
Joaquin Ossandón <jiossandon@uc.cl>
2019-09-22 22:31:15 -0300

    Adding new column

--- filtering_liq.py
+++ filtering_liq.py
@@ -12,9 +12,12 @@ file.close()
 def read_file_liq(filename):
     return pd.read_csv(f"Stocks/{filename}", index_col='Date', parse_dates=True, usecols=['Date', 'Close', 'Volume'])
 
-dataframe_filtered = None
-for file in data:
-    pass
+
+for file in data[:100]:
+    df = read_file_liq(file)
+    df["VolumeporPrice"] = df["Volume"] * df["Close"]
+    porcentaje = sum([1 for x in df["VolumeporPrice"] if x < 500])/len(df["Volume"]) * 100
+    print(f"{file}: %{porcentaje}")
 
 file = open('listof_files/list_of_files_4_years_liqu', 'wb')
 # dump information to that file
